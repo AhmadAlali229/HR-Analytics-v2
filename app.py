e1de3ce72f605b83cb7660ac7786c5eaaa0f9c4c
AhmadAlali229 <ahmadalali229229@gmail.com>
2025-09-18 13:46:29 +0300

    app: set page config, title/description, and create tabs (Overview/Add/Update)

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ def load_employees():
     return pd.read_sql_query("SELECT * FROM employees;", conn)
 
 df = load_employees()
+st.set_page_config(page_title="HR Dashboard", page_icon="📊", layout="wide")# Title
+st.markdown('<h1 style="font-size:50px; font-weight:800; margin-bottom:10px;">HR Analytics Dashboard</h1>', unsafe_allow_html=True)#Styling Title
+st.markdown("An interactive dashboard where you can analyze employees, add new records, and update salaries easily.")#Descreption
+
+tab_overview, tab_add, tab_update = st.tabs(["Overview", "Add Employee", "Update Income"])#Creating Taps to be usesd later
